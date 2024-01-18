67bc8b615dffa66c7e840ca588291cf4805eb82a
Steven Jagurić <steven.jaguric@gmail.com>
2024-01-19 00:36:37 +0100

    Update categories.js
    Will fill it out more later

---
+++ croatian-cues-100/src/data/categories.js
@@ -0,0 +1,23 @@
+const categories = [
+    {
+        id: 1,
+        name: "People",
+        words: [
+            { id: 1, english: "Baby", croatian: "Beba" },
+        ],
+    },
+    {
+        id: 2,
+        name: "Colors and Shapes",
+        words: [
+            { id: 1, english: "Green Rectangle", croatian: "Zeleni Pravokutnik" },
+        ],
+    },
+    {
+        id: 3,
+        name: "Toys",
+        words: [
+            { id: 1, english: "Teddy Bear", croatian: "Medo" },
+        ],
+    },
+]
