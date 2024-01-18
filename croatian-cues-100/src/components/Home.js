1aa4c57c4356f5f226ecf7ae8224da1d2736f12b
Steven Jagurić <steven.jaguric@gmail.com>
2024-01-19 00:40:47 +0100

    Update Home.js

---
+++ croatian-cues-100/src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Buttons } from './Buttons';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Home</h1>
+            <Buttons />
+        </div>
+    );
+};
+
+export default Home;
