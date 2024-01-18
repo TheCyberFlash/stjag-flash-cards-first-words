14ba8ae430f5586e73c3f391fcc425a242d30d07
Steven Jagurić <steven.jaguric@gmail.com>
2024-01-19 00:44:26 +0100

    Update CardCollection.js

---
+++ croatian-cues-100/src/components/CardCollection.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { FlashCard } from "./FlashCard";
+
+const CardCollection = () => {
+    const selectedCategory = useSelector((state) => state.selectedCategory);
+
+    if (!selectedCategory) {
+        return <div>Please select a category</div>;
+    }
+
+    return (
+        <div>
+            <h2>{selectedCategory.name}</h2>
+            {selectedCategory.words.map((word) => (
+                <FlashCard key={word.id} word={word} />
+            ))}
+        </div>
+    )
+}
+
+export default CardCollection;
