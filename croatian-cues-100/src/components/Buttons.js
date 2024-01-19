2b924d9af6003351b680e75b36c96b3e8df64d99
Steven Jagurić <steven.jaguric@gmail.com>
2024-01-19 02:02:11 +0100

    Update Buttons.js

--- croatian-cues-100/src/components/Buttons.js
+++ croatian-cues-100/src/components/Buttons.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate  } from 'react-router-dom';
 import { categories } from '../data/categories';
 import { useDispatch } from 'react-redux';
 import { selectCategory } from '../redux/actions';
 
 const Buttons = () => {
     const dispatch = useDispatch();
+    const navigate = useNavigate ();
+
+    const handleRandomClick = () => {
+        const randomCategory = categories[Math.floor(Math.random() * categories.length)];
+        dispatch(selectCategory(randomCategory));
+        navigate(`/category/${randomCategory.id}`);
+    };
 
     return (
         <div>
@@ -18,6 +25,7 @@ const Buttons = () => {
                     <button>{category.name}</button>
                 </Link>
             ))}
+                        <button onClick={handleRandomClick}>Random</button>
         </div>
     );
 };
