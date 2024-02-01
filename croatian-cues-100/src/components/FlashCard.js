0f097baf7c730ba4973989cbe2b5ad0e0ef5989a
Steven Jagurić <steven.jaguric@gmail.com>
2024-02-01 18:31:45 +0100

    Update FlashCard.js

--- croatian-cues-100/src/components/FlashCard.js
+++ croatian-cues-100/src/components/FlashCard.js
@@ -1,25 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const FlashCard = ({ word }) => {
-    const [isFlipped, setIsFlipped] = useState(false);
+  const [isFlipped, setIsFlipped] = useState(false);
 
-    const handleClick = () => {
-        setIsFlipped(!isFlipped);
-    };
+  const handleClick = () => {
+    setIsFlipped(!isFlipped);
+  };
 
-    const getImageSrc = (imageName) => {
-        return `${process.env.PUBLIC_URL}/images/${imageName}.png`;
-    };
-      
+  const getImageSrc = (imageName) => {
+    return `${process.env.PUBLIC_URL}/images/${imageName}.png`;
+  };
 
-    return (
-        <div className={`flip-card ${isFlipped ? 'flipped' : ''}`} onClick={handleClick}>
-            <div className="flip-card-inner">
-                <img className="card-image" src={getImageSrc(word.english)} alt={word.english} />
-                <div className="card-text">{isFlipped ? word.english : word.croatian}</div>
-            </div>
+  return (
+    <div
+      className={`flip-card ${isFlipped ? "flipped" : ""}`}
+      onClick={handleClick}
+    >
+      <div className="flip-card-inner">
+        <img
+          className="card-image"
+          src={getImageSrc(word.english)}
+          alt={word.english}
+        />
+        <div className="card-text">
+          {isFlipped ? word.english : word.croatian}
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default FlashCard;
