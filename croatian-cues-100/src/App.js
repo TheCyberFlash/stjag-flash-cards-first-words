5e302d9aafaffcd1ecd7cf8b5c3cc1c5d0474aa7
Steven Jagurić <steven.jaguric@gmail.com>
2024-02-01 18:32:06 +0100

    Update App.js

--- croatian-cues-100/src/App.js
+++ croatian-cues-100/src/App.js
@@ -4,14 +4,14 @@ import Home from "./components/Home";
 import CardCollection from "./components/CardCollection";
 
 const App = () => {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/category/:id" element={<CardCollection />} />
-            </Routes>
-        </Router>
-    );
-}
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/category/:id" element={<CardCollection />} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
